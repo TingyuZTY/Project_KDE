d0cc9e495712f29f937d79279ea5347f60e62ec5
TingyuZTY <zhuti@oregonstate.edu>
2020-12-07 02:14:22 -0800

    #8 Document three primary functions

--- R/kde_est_functions.R
+++ R/kde_est_functions.R
@@ -10,16 +10,31 @@ kernel_tri <- function(x){(1-x^2)^3*(abs(x)<=1)/beta(4,0.5)} # triweight kernel
 ###############################
 # Define the KDE function   #
 ##############################
+
+#' Generate the Kernel Density Estimation results
+#'
+#' @param x numeric vector, the samples;  
+#' @param grid numeric vector, the grid points;
+#' @param h positive numeric, the bandwidth; 
+#' @param ker character, name of the kernel function.
+#'
+#' @return list of esimated densities and corresponding grid points.
+#' @export
+#'
+#' @examples
+#'KDE_est(x=rnorm(50),grid=seq(-2,2, length.out = 100),h=NULL,ker="normal")
 KDE_est <- function(x,grid=NULL,h,ker="normal"){
-  # inputs: 
-  # x=sample; h=bandwidth; grid=grid point; ker=kernel 
+  #- inputs: 
+  #  - x: numeric vector, the samples;  
+  # - grid: numeric vector, the grid points;
+  # - h: positive numeric, the bandwidth; 
+  # - ker: character, name of the kernel function.
+  # - outputs: list of esimated densities and corresponding grid points.
+  
   if(is.null(grid)) {
-    grid = seq(min(x)-3*h, max(x)+3*h, length.out=512)
+    grid <-  seq(min(x)-3*h, max(x)+3*h, length.out=512)
   }
   
-  if(is.null(h)) {
-    h = 1.06*sd(x)*nx^(-0.2)
-  }
   
   if(ker=="normal") ker <- kernel_norm
   else if(ker=="uniform") ker <- kernel_uniform
@@ -28,12 +43,16 @@ KDE_est <- function(x,grid=NULL,h,ker="normal"){
   else if(ker=="triweight") ker <- kernel_tri
   
   ## Generate grid point matrix
-  ng<-length(grid)
-  nx<-length(x)
+  ng <- length(grid)
+  nx <- length(x)
   x0 <- matrix(rep(1,nx*ng), nx,ng)
   
   x_mat <- x*x0
   x_grid <- grid*t(x0)
+  
+  if(is.null(h)) {
+    h <-  1.06*sd(x)*nx^(-0.2)
+  }
   
   ## kernel estimation
   x_ker <- x_mat - t(x_grid)
@@ -51,15 +70,29 @@ made <- function(f_est,f_true) mean(abs(f_est-f_true))
 # And calculate the MADE under difference settings.                                        #
 #############################################################################################
 
-#`kde_est_bid()` is to simulate over different sample sizes & kernels & bandwidths. 
-# The output is the estimated densities. 
-
+#' Kernel density estimation simulations over given sample size, kernel, and bandwidth
+#'
+#' @param x numeric vector, the large population;
+#' @param n positive numeric, the sample size;
+#' @param ker character, name of the kernel function;
+#' @param h positive numeric, the bandwidth;
+#' @param grid numeric vector, the grid points.
+#'
+#' @return vector of the estimated densities. 
+#' @export
+#'
+#' @examples 
+#' kde_est_big(x=rnorm(500), n=100,ker="normal", h=0.5, grid=seq(-2,2, length.out = 100))
 kde_est_big <- function(x, n, ker, h, grid){
-  # x: large population
-  # n: sample sizes
-  # ker: name of kernels
-  # h: bandwidths 
-  # grid grid points
+  
+  #`kde_est_bid()` is to simulate over given sample size & kernel & bandwidth. 
+  # - inputs:
+  # - x: numeric vector, the large population;
+  # - n: positive numeric, the sample size;
+  # - ker: character, name of the kernel function;
+  # - h: positive numeric, the bandwidth;
+  # - grid: numeric vector, the grid points.
+  # - output: vector of the estimated densities. 
   
   # sample from the population x with different sample size n
   l <- data.frame(n=n, ker=ker, h=h) %>% 
@@ -67,18 +100,49 @@ kde_est_big <- function(x, n, ker, h, grid){
   l$n <- NULL
   
   # generate estimated densities with the sample of size n & ker & h
-  est <- pmap_dfr(l, KDE_est, grid=grid)
+  est <-  purrr::pmap_dfr(l, KDE_est, grid=grid)
   est$f_est
 }
 
 
-# `sim_made` is to first generate combination of parameters, 
-# and then for each combination of parameters, 
-# using `kde_est_big()` to generate the estimated densities, and the corresponding true densities. 
-# Then, `made()` is used to computae the MADE. 
-# The out out is the MADEs under different combination of parameters.
-
-sim_big_made <- function(x, ns, kes, h, grid, true_f){
+#' Kernel density estimation simulations over differnt combinations of sample sizes, kernels, and bandwidths
+#'
+#' @param x numeric vector, the large population;  
+#' @param ns numeric vector, different sample sizes;
+#' @param kers character vector, names of different kernel functions;
+#' @param hs positive numeric data.frame, different bandwidths;
+#' @param grid numeric vector, the grid points;
+#' @param true_f 
+#'
+#' @return list of MADEs under different combination of parameters.
+#' @export
+#'
+#' @examples
+#' # parameter setting
+#' x <- rnorm(500)
+#' ns <- c(100, 200)
+#' kers <- c("normal","biweight")
+#' hs <- data.frame(h1=0.1,h2=0.5)
+#' grid <- seq(-2,2, length.out = 100)
+#' true_f <- dnorm(grid)
+#' 
+#' sim_big_made(x, ns, kers, hs, grid, true_f)
+sim_big_made <- function(x, ns, kers, hs, grid, true_f){
+  # `sim_made` is used to first generate combination of parameters. 
+  # Then, for each combination of parameters, `kde_est_big()` is used to 
+  # generate the estimated densities, and the corresponding true densities. 
+  # The `made()` is used to computae the Mean Absolute Deviation Errors(MADE). 
+  
+  # - inputs:
+  # - x: numeric vector, the large population;  
+  # - ns: numeric vector, different sample sizes;
+  # - kers: character vector, names of different kernel functions;
+  # - hs: positive numeric data.frame, different bandwidths;
+  # - grid: numeric vector, the grid points;
+  # - true_f: numeric vector, true densities.
+  # - output: list of MADEs under different combination of parameters.
+  # 
+  
   # generates every combination of parameters (ns:kers:hs)
   simulation_params_big <- list(
     n = ns,
@@ -86,16 +150,16 @@ sim_big_made <- function(x, ns, kes, h, grid, true_f){
     h = hs)
   
   est_big <- cross_df(simulation_params_big)
-  r <- pmap(list(n=100,ker="normal", h=0.1), kde_est_big,x, grid)
+  r <-  purrr::pmap(list(n=100,ker="normal", h=0.1), kde_est_big,x, grid)
  
   est_big <- est_big %>% 
     mutate(
       # using `kde_est_big()` to generate the estimated densities
-      f_ests = pmap(list(n=n,ker=kernel_type, h=h), kde_est_big,x, grid),
-      f_true = map(1:length(n), ~true_f)# the corresponding true densities. 
+      f_ests =  purrr::pmap(list(n=n,ker=kernel_type, h=h), kde_est_big,x, grid),
+      f_true =  purrr::map(1:length(n), ~true_f)# the corresponding true densities. 
     ) %>% 
     mutate(
-      MADE = map2_dbl(.x = f_ests, .y = f_true, ~made(.x,.y))# `made()` is used to computae the MADE. 
+      MADE =  purrr::map2_dbl(.x = f_ests, .y = f_true, ~made(.x,.y))# `made()` is used to computae the MADE. 
     )
   return(est_big$MADE)
 }
@@ -105,6 +169,13 @@ sim_big_made <- function(x, ns, kes, h, grid, true_f){
 # Functions that can save the result of the "sim_big_made" function in two forms.           #
 ##############################################################################################
 save_made <- function(results,ns,kers,hs,n.sim){
+  # - inputs:
+  # - results: result generated from the `sim_big_made()`
+  # - ns: numeric vector, different sample sizes;
+  # - kers: character vector, names of different kernel functions;
+  # - hs: positive numeric vector, different bandwidths;
+  # - n.sim: positive numeric, number of replicates.
+  # - output: list of made_mat_big and  made_comapre 
   # made_mat_big saves the raw simulation data 
   # made_comapre saves the mean and sd of all replications under 
   # different combianation of parameters that can be used to generate plots
@@ -125,9 +196,9 @@ save_made <- function(results,ns,kers,hs,n.sim){
 
   made_comapre <- made_comapre %>% mutate(
     # add the number of simulations 
-    n.sim = rep( n.sim, length(n)),
+    n.sim = rep(n.sim, length(n)),
     # add the names(types) of the bandwidths
-    bandwidth = map_chr(.x=h, ~names(hs)[which(.x == hs)])
+    bandwidth =  purrr::map_chr(.x=h, ~names(hs)[which(.x == hs)])
   ) 
 
   return(list(made_mat_big=made_mat_big,made_comapre=made_comapre ))
